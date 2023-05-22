589941050e181e3b09f33bb77a7fe3ab40c5e775
vaparova <vaparova_92@hotmail.com>
2023-05-22 18:24:16 -0300

    2

--- src/app/components/componente-dos/componente-dos.component.ts
+++ src/app/components/componente-dos/componente-dos.component.ts
@@ -12,14 +12,17 @@ export class ComponenteDosComponent implements OnInit {
   url = 'https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key='
   key = 'live_ssY9l2168QNh0FjlJVVmj4Nw23DQrgWzKCxLTfG96yi96UaVcsuPusbkVCNlcj9A';
   gato: Gato;
+  arrGatos: Gato[] = [];
   
 
-  constructor(private gatos: GatoService) {
+  constructor(private gatoServ: GatoService) {
     this.gato = new Gato('12k',
     'https://thecatapi.com/api/images/get?format=src&type=gif?results_per_page='
     ,250, 250);
 
-    console.log(this.gatos.getGato());
+    this.arrGatos = this.gatoServ.getGato();
+
+    console.log(this.arrGatos);
     
    }
 
--- src/app/providers/gato.service.ts
+++ src/app/providers/gato.service.ts
@@ -1,27 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Gato } from '../models/gato';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Observer } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GatoService {
 
-  gatos: Gato[] = [
-    {id:"3cm",url:"https://cdn2.thecatapi.com/images/3cm.jpg",width:612,height:612},
-    {id:"7bd",url:"https://cdn2.thecatapi.com/images/7bd.gif",width:500,height:282},
-    {id:"7kg",url:"https://cdn2.thecatapi.com/images/7kg.png",width:435,height:318},
-    {id:"7ls",url:"https://cdn2.thecatapi.com/images/7ls.png",width:500,height:390},
-    {id:"e1r",url:"https://cdn2.thecatapi.com/images/e1r.jpg",width:720,height:576},
-    {id:"edd",url:"https://cdn2.thecatapi.com/images/edd.jpg",width:1024,height:672},
-    {id:"efm",url:"https://cdn2.thecatapi.com/images/efm.gif",width:500,height:223},
-    {id:"MTQ5MzcyNA",url:"https://cdn2.thecatapi.com/images/MTQ5MzcyNA.jpg",width:481,height:720},
-    {id:"i-j1kCi3b",url:"https://cdn2.thecatapi.com/images/i-j1kCi3b.jpg",width:1280,height:720},
-    {id:"StMwZZnDP",url:"https://cdn2.thecatapi.com/images/StMwZZnDP.jpg",width:1440,height:900}
-  ];
+  gatos: any = [];
 
-  constructor() { }
+  constructor( private httpClient: HttpClient) {
+    this.consumirGatos();
+  }
 
   getGato(): Gato[]{
     return this.gatos;
+  }
+
+  consumirGatos() {
+    this.httpClient.get('https://api.thecatapi.com/v1/images/search?limit=10').subscribe((data) => {
+    Array(data).forEach((gato: any) => {
+      console.log(gato);
+      gato.forEach((cat:any) => {
+        this.gatos.push(cat);
+      });
+    }); 
+    });
   }
 }
