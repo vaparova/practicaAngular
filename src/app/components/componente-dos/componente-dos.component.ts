e155a9223c10883411d4a5c93217d945db23893b
vaparova <vaparova_92@hotmail.com>
2023-05-22 18:46:23 -0300

    2

--- src/app/components/componente-dos/componente-dos.component.ts
+++ src/app/components/componente-dos/componente-dos.component.ts
@@ -8,7 +8,7 @@ import { GatoService } from 'src/app/providers/gato.service';
   templateUrl: './componente-dos.component.html',
   styleUrls: ['./componente-dos.component.css']
 })
-export class ComponenteDosComponent implements OnInit {
+export class ComponenteDosComponent  {
   url = 'https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key='
   key = 'live_ssY9l2168QNh0FjlJVVmj4Nw23DQrgWzKCxLTfG96yi96UaVcsuPusbkVCNlcj9A';
   gato: Gato;
@@ -26,9 +26,6 @@ export class ComponenteDosComponent implements OnInit {
     
    }
 
-  ngOnInit(): void {
-  
-  }
 
   cambiarGato(){
     this.gato.url = this.gato.url + 1;
